2f395182d156b94e8433a9f09050d1b4b0f54c29
fahmyfauzi <fahmyfauzii@gmail.com>
2023-10-19 10:57:51 +0700

    create, setup project and entities

---
+++ app/src/main/java/com/csm/roomdatabase/data/models/Word.kt
@@ -0,0 +1,13 @@
+package com.csm.roomdatabase.data.models
+
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(indices = [Index(value = ["word"], unique = true)]) //index/ndces untuk mempercepat query
+data class Word(
+    @PrimaryKey(autoGenerate = true) val id:Int,
+    val word:String,
+    @Ignore val counter:Int
+)
